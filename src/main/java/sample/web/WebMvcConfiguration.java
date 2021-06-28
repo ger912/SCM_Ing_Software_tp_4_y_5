4189a2096a57af7be51b28ed3bbee0f4e70a4fe9
German <gercoss.92@gmail.com>
2021-06-27 21:01:50 -0300

    springboot error bla bla

--- src/main/java/sample/web/WebMvcConfiguration.java
+++ src/main/java/sample/web/WebMvcConfiguration.java
@@ -1,7 +1,7 @@
 package sample.web;
 
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.web.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
