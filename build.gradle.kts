b83095b8fe97b0f45669146ed19c704cefa489b6
German <gercoss.92@gmail.com>
2021-07-02 13:30:42 -0300

    saco jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("org.springframework.boot") version "2.5.2"
     id("org.seasar.doma.compile") version "1.1.0"
     id("org.sonarqube") version "3.2.0"
-	id("jacoco")
 }
 
 apply(plugin = "io.spring.dependency-management")
@@ -85,11 +84,5 @@ eclipse {
 tasks {
     test {
         useJUnitPlatform()
-    }
-}
-
-jacocoTestReport {
-    reports {
-        xml.enabled true
     }
 }
